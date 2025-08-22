447828b38c975211ffcf1111c6801c2727f3f398
bernardopadilha <bernardo_limas@hotmail.com>
2025-08-21 21:28:16 -0300

    feat: adicionado condição para quando tiver um total de 0 produtos cadastrados

--- src/components/new-product-btn.tsx
+++ src/components/new-product-btn.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils'
 import { PlusSignIcon } from '@hugeicons/core-free-icons'
 import { HugeiconsIcon } from '@hugeicons/react'
 import { Button } from './ui/button'
@@ -5,18 +6,20 @@ import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip'
 
 export default function NewProductBtn({
   align = 'end',
+  className,
 }: {
   align?: 'center' | 'end' | 'start' | undefined
+  className?: string
 }) {
   return (
     <Tooltip delayDuration={7000}>
       <TooltipTrigger asChild>
-        <Button className="font-normal">
+        <Button className={cn('font-normal', className)}>
           <HugeiconsIcon icon={PlusSignIcon} className="size-[14px]" />
           Novo produto
         </Button>
       </TooltipTrigger>
-      <TooltipContent className="" align={align} side="bottom">
+      <TooltipContent align={align} side="bottom">
         <p className="text-primary-foreground">
           Tá esperando o quê? Boraa moeer!! 🚀
         </p>
--- src/pages/(app)/products.tsx
+++ src/pages/(app)/products.tsx
@@ -1,28 +1,54 @@
+import NewProductBtn from '@/components/new-product-btn'
 import { products } from '@/features/dashboard/mock/products'
 import FilterProductsCard from '@/features/products/components/filter-products-card'
 import ProductCard from '@/features/products/components/product-card'
+import { AddCircleHalfDotIcon } from '@hugeicons/core-free-icons'
+import { HugeiconsIcon } from '@hugeicons/react'
 
 export default function ProductsPage() {
   return (
     <div className="flex-1 flex py-16 justify-center">
       <div className="flex flex-col max-w-6xl w-full">
-        <div className="space-y-2">
-          <h1 className="font-secondary text-2xl font-bold">Seus produtos</h1>
-          <p className="text-sm">
-            Acesse gerencie a sua lista de produtos à venda
-          </p>
-        </div>
+        {products.length <= 0 ? (
+          <div className="w-full flex flex-col items-center justify-center gap-5">
+            <div className="space-y-1">
+              <h1 className="font-secondary text-2xl font-bold">
+                Cadastre seu primeiro produto
+              </h1>
+              <p className="text-sm">
+                Você ainda não possui produtos cadastrados
+              </p>
+            </div>
 
-        <div className="flex flex-col lg:flex-row gap-6 mt-10">
-          <div className="sticky">
-            <FilterProductsCard />
+            <HugeiconsIcon
+              icon={AddCircleHalfDotIcon}
+              className="size-10 text-primary"
+            />
+
+            <NewProductBtn align="center" className="h-14 w-56" />
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2  gap-5">
-            {products.map((product, i) => (
-              <ProductCard {...product} key={i} />
-            ))}
-          </div>
-        </div>
+        ) : (
+          <>
+            <div className="space-y-2">
+              <h1 className="font-secondary text-2xl font-bold">
+                Seus produtos
+              </h1>
+              <p className="text-sm">
+                Acesse gerencie a sua lista de produtos à venda
+              </p>
+            </div>
+            <div className="flex flex-col lg:flex-row gap-6 mt-10">
+              <div className="sticky top-10 h-fit self-start">
+                <FilterProductsCard />
+              </div>
+              <div className="grid grid-cols-1 sm:grid-cols-2  gap-5">
+                {products.map((product, i) => (
+                  <ProductCard {...product} key={i} />
+                ))}
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
